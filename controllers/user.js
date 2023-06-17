f545e3a9bd2d58523ccac256429e491f08689f19
Ilya Novikov <ilnovikov@ilnovikov.ru>
2023-06-17 10:14:03 +0100

    version 1.4-b

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,35 @@
+const mongoose = require('mongoose');
 const User = require('../models/user');
+
+exports.getUsers = (req, res, next) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch(next);
+};
+
+exports.getUserById = (req, res, next) => {
+  const { userId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(400).send({ message: 'Некорректный id пользователя' });
+  }
+
+  return User.findById(userId).select('name about avatar _id')
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Пользователь не найден' });
+      }
+      return res.status(200).send(user);
+    })
+    .catch(next);
+};
+
+exports.createUser = (req, res, next) => {
+  const { name, about, avatar } = req.body;
+  const user = new User({ name, about, avatar });
+  user.save()
+    .then((newUser) => res.status(201).send(newUser))
+    .catch(next);
+};
 
 module.exports.updateProfile = (req, res, next) => {
   const { name, about } = req.body;
--- routes/user.js
+++ routes/user.js
@@ -1,50 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const {
+  getUsers, getUserById, createUser, updateProfile, updateAvatar,
+} = require('../controllers/user');
 
 const router = express.Router();
-const User = require('../models/user');
-const { updateProfile, updateAvatar } = require('../controllers/user');
 
-router.get('/users', (req, res) => {
-  User.find({})
-    .then((users) => res.status(200).send(users))
-    .catch((err) => res.status(500).send({ message: 'Произошла ошибка', error: err }));
-});
-
-router.get('/users/:userId', (req, res) => {
-  const { userId } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(userId)) {
-    return res.status(400).send({ message: 'Некорректный id пользователя' });
-  }
-
-  return User.findById(userId).select('name about avatar _id')
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send({ message: 'Пользователь не найден' });
-      }
-      return res.status(200).send(user);
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Некорректный id пользователя' });
-      }
-      return res.status(500).send({ message: 'Произошла ошибка', error: err });
-    });
-});
-
-router.post('/users', (req, res) => {
-  const { name, about, avatar } = req.body;
-  const user = new User({ name, about, avatar });
-  user.save()
-    .then((newUser) => res.status(201).send(newUser))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Ошибка валидации', error: err });
-      }
-      return res.status(500).send({ message: 'Произошла ошибка', error: err });
-    });
-});
-
+router.get('/users', getUsers);
+router.get('/users/:userId', getUserById);
+router.post('/users', createUser);
 router.patch('/users/me', updateProfile);
 router.patch('/users/me/avatar', updateAvatar);
 
