e32e61163fb906eb189552c6235a040bc050b1b0
Ilya Novikov <ilnovikov@ilnovikov.ru>
2023-06-30 16:11:17 +0100

    version 1.8-b

--- routes/card.js
+++ routes/card.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 
 const router = express.Router();
@@ -5,10 +6,28 @@ const {
   createCard, getCards, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/card');
 
+const { celebrate, Joi } = require('celebrate');
+
+const validateObjId = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().custom((value, helpers) => {
+      if (mongoose.Types.ObjectId.isValid(value)) {
+        return value;
+      }
+      return helpers.message('Невалидный id');
+    }),
+  }),
+});
+
 router.get('/cards', getCards);
-router.post('/cards', createCard);
-router.delete('/cards/:cardId', deleteCard);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().uri(),
+  }),
+}), createCard);
+router.delete('/cards/:id', validateObjId, deleteCard);
+router.put('/cards/:id/likes', validateObjId, likeCard);
+router.delete('/cards/:id/likes', validateObjId, dislikeCard);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,22 @@ const { getUsers, getUserById, getUserInfo } = require('../controllers/user');
 
 const router = express.Router();
 
+const { celebrate, Joi } = require('celebrate');
+const mongoose = require('mongoose');
+
+const validateObjId = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().custom((value, helpers) => {
+      if (mongoose.Types.ObjectId.isValid(value)) {
+        return value;
+      }
+      return helpers.message('Невалидный id');
+    }),
+  }),
+});
+
 router.get('/users', getUsers);
-router.get('/users/:userId', getUserById);
+router.get('/users/:id', validateObjId, getUserById);
 router.get('/users/me', getUserInfo);
 
 module.exports = router;
