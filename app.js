59b2aa53d627c436f23f13739feaccd401892cb5
Ilya Novikov <ilnovikov@ilnovikov.ru>
2023-07-05 20:50:14 +0100

    version 2.39-b

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const { login, createUser } = require('./controllers/user');
-const userRoutes = require('./routes/user');
-const cardRoutes = require('./routes/card');
-const auth = require('./middlewares/auth');
-const { signinValidation, signupValidation } = require('./validators');
 
-const UnauthorizedError = require('./errors/UnauthorizedError');
 const NotFoundError = require('./errors/NotFoundError');
+const routes = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.post('/signin', signinValidation, login);
-app.post('/signup', signupValidation, createUser);
-
-app.use(auth);
-app.use(userRoutes);
-app.use(cardRoutes);
-
-// eslint-disable-next-line no-unused-vars
 app.use((req, res, next) => {
   next(new NotFoundError('Запрашиваемый ресурс не найден'));
 });
 
 app.use(errors());
-
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Произошла ошибка на сервере';
-  res.status(statusCode).send({ message });
-});
+app.use(errorHandler);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
